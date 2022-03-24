4ce5ba1dd7db6068c131c6ac838b687860f6ab7a
Hyttel <mikkel.hw@hotmail.com>
2022-03-24 09:53:46 +0100

    start

---
+++ src/main/java/com/example/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,62 @@
+package com.example.pokemon.repository;
+
+import com.example.pokemon.model.Pokemon;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PokemonRepository {
+
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/pokedex";
+    private final static String UID = "pokemon_master";
+    //<editor-fold desc="Description">
+    private final static String PWD = "pokemon";
+    //</editor-fold>
+    private static Connection connection;
+
+    public static void getConnection() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            System.out.println("Connected succesfully");
+        } catch (SQLException e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
+    }
+
+    public List<Pokemon> readAll() {
+        final String READ_SQL = "SELECT * FROM pokemon";
+        List<Pokemon> pokemonList = new ArrayList<>();
+        getConnection();
+        try {
+            Statement statement = connection.prepareStatement(READ_SQL);
+            ResultSet resultSet = statement.executeQuery(READ_SQL);
+            System.out.println("\nPokedex:\n");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("pokedex_number");
+                String name = resultSet.getString("name");
+                int speed = resultSet.getInt("speed");
+                int special_defence = resultSet.getInt("special_defence");
+                int special_attack = resultSet.getInt("special_attack");
+                int defence = resultSet.getInt("defence");
+                int attack = resultSet.getInt("attack");
+                int hp = resultSet.getInt("hp");
+                String primary_type = resultSet.getString("primary_type");
+                String secondary_type = resultSet.getString("secondary_type");
+
+                System.out.println("Pokedex number: " + resultSet.getInt("pokedex_number") + "\n" + "Name: "
+                        + resultSet.getString("name") + "\n");
+                pokemonList.add(new Pokemon(id, name, speed, special_defence, special_attack, defence, attack, hp, primary_type, secondary_type));
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
+        return pokemonList;
+    }
+
+}
